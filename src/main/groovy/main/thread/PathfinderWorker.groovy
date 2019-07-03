9b2ed39cc5417245dd226a3323b8f7337dcc8341
Per Eriksson <pere5@kth.se>
2019-07-03 21:50:35 +0200

    hell yeah!

--- src/main/groovy/main/thread/PathfinderWorker.groovy
+++ src/main/groovy/main/thread/PathfinderWorker.groovy
@@ -168,8 +168,8 @@ class PathfinderWorker extends Worker {
         for (int i = deltaIdx + 1; i < deltaIdx + ctl.size(); i++) {
             def n = [currentStep[0] + ctl.get(i)[0], currentStep[1] + ctl.get(i)[1]] as int[]
             def tile = tileNetwork[n[0]][n[1]]
-            if (n != previousStep
-                    && n != nextStep
+            if (n == previousStep) break
+            if (n != nextStep
                     && villager.canTravel(tile.travelType)
                     && !visited.contains([n[0], n[1]])
             ) {
@@ -181,8 +181,8 @@ class PathfinderWorker extends Worker {
         for (int i = deltaIdx - 1; i > deltaIdx - ctl.size(); i--) {
             def n = [currentStep[0] + ctl[i][0], currentStep[1] + ctl[i][1]] as int[]
             def tile = tileNetwork[n[0]][n[1]]
-            if (n != previousStep
-                    && n != nextStep
+            if (n == previousStep) break
+            if (n != nextStep
                     && villager.canTravel(tile.travelType)
                     && !visited.contains([n[0], n[1]])
             ) {
