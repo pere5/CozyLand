5ecf757c4a4c077a20528d18458cd0ff9185e0f4
Per Eriksson <pere5@kth.se>
2019-07-21 15:17:07 +0300

    one test, need more

--- src/main/groovy/main/model/Tile.groovy
+++ src/main/groovy/main/model/Tile.groovy
@@ -1,9 +1,19 @@
 package main.model
 
+import main.Main
 import main.Model
 import main.things.Drawable
 
 class Tile extends Drawable {
+
+    static Tile w(int x, int y) {
+        new Tile(height: 10, size: Main.TILE_WIDTH, x: x, y: y, travelType: Model.TravelType.WATER)
+    }
+
+    static Tile p(int x, int y) {
+        new Tile(height: 10, size: Main.TILE_WIDTH, x: x, y: y, travelType: Model.TravelType.PLAIN)
+    }
+
     int height = 0
     Model.TravelType travelType
 }
--- src/test/groovy/main/Tests2.groovy
+++ src/test/groovy/main/Tests2.groovy
@@ -33,10 +33,15 @@ class Tests2 {
     @Test
     void findPath() {
         Model.tileNetwork = [
-                [new Tile(height: 10, size: sw, x: 0, y: 0, travelType: p), new Tile(height: 10, size: sw, x: 0, y: 1, travelType: w)]
+                [Tile.p(0,0), Tile.p(0,1), Tile.p(0,2), Tile.p(0,3)],
+                [Tile.p(1,0), Tile.p(1,1), Tile.w(1,2), Tile.w(1,3)],
+                [Tile.p(2,0), Tile.w(2,1), Tile.w(2,2), Tile.w(2,3)],
+                [Tile.p(3,0), Tile.w(3,1), Tile.w(3,2), Tile.w(3,3)]
         ]
         def (int[] left, int[] right) = new PathfinderWorker().findPath([2,2] as int[], [1,1] as int[], [0,0] as int[], [] as Set<List<Integer>>, new Villager())
 
+        assert left == [0, 2] as int[]
+        assert right == [2, 0] as int[]
         //do stuff here
     }
 }
