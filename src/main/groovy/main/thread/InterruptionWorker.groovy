ef6b8ead8d3e9eaf7ac18c814f9b53d52fefee23
Per Eriksson <pere5@kth.se>
2020-07-18 12:34:50 +0200

    Interruption

--- src/main/groovy/main/thread/InterruptionWorker.groovy
+++ src/main/groovy/main/thread/InterruptionWorker.groovy
@@ -2,8 +2,14 @@ package main.thread
 
 class InterruptionWorker extends Worker {
 
-    def update() {
+    @Override
+    def run() {
+        super.intendedFps = 3
+        super.run()
+    }
 
+    def update() {
+        //todo: interrupt if new role, abort all actions
 
     }
 }
