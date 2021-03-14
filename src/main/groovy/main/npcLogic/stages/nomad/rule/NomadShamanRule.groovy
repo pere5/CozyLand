966cfe50046a446dc61383cc99062a5ee655cdec
Eriksson <pere5@kth.se>
2021-03-14 14:47:12 +0000

    min tribe size for hamlet

--- src/main/groovy/main/npcLogic/stages/nomad/rule/NomadShamanRule.groovy
+++ src/main/groovy/main/npcLogic/stages/nomad/rule/NomadShamanRule.groovy
@@ -14,8 +14,9 @@ import main.utility.Utility
 
 class NomadShamanRule extends Rule {
 
-    private static final int NUMBER_SURVEYS = 2
-    private static final int MIN_UNIQUE = 2
+    private static final int MIN_SURVEYS = 2
+    private static final int MIN_UNIQUE_RESOURCES = 2
+    private static final int MIN_VILLAGERS = 5
 
     NomadShamanRule(int rank) {
         this.rank = rank
@@ -27,44 +28,46 @@ class NomadShamanRule extends Rule {
             return GOOD
         } else if (me.metaObjects[NomadShamanRule.toString()]) {
             return BAD
+        } else if (me.role.tribe.villagers.size() < MIN_VILLAGERS) {
+            return BAD
+        } else if (me.role.tribe.surveyNaturalResources.size() < MIN_SURVEYS) {
+            return BAD
         } else {
             def surveyResources = me.role.tribe.surveyNaturalResources
             List<Location> goodLocations = []
-            if (surveyResources.size() > NUMBER_SURVEYS) {
-                def maxUnique = surveyResources.collect {
-                    it.value.unique(false) { it.shape }.size()
-                }.max()
-                if (maxUnique >= MIN_UNIQUE) {
 
-                    surveyResources.each { def spot, Set<NaturalResource> resources ->
-                        def multiplier = 1
-                        def counted = resources.countBy {
-                            it.shape
-                        }
-                        def list = counted.collect {
-                            [shape: it.key, amount: it.value]
-                        }
-                        def sorted = list.sort {
-                            -it.amount
-                        }
-                        def multiplied = sorted.collect {
-                            it.amount *= multiplier
-                            multiplier *= 3
-                            return it
-                        }
-                        def score = multiplied.sum {
-                            it.amount
-                        } as Integer
-                        goodLocations << new Location(spot: spot as int[], naturalResources: resources, score: score)
+            def uniqueResourcesAtSpot = surveyResources.collect {
+                it.value.unique(false) { it.shape }.size()
+            }.max()
+
+            if (uniqueResourcesAtSpot < MIN_UNIQUE_RESOURCES) {
+                return BAD
+            } else {
+                surveyResources.each { def spot, Set<NaturalResource> resources ->
+                    def multiplier = 1
+                    def counted = resources.countBy {
+                        it.shape
                     }
+                    def list = counted.collect {
+                        [shape: it.key, amount: it.value]
+                    }
+                    def sorted = list.sort {
+                        -it.amount
+                    }
+                    def multiplied = sorted.collect {
+                        it.amount *= multiplier
+                        multiplier *= 3
+                        return it
+                    }
+                    def score = multiplied.sum {
+                        it.amount
+                    } as Integer
+                    goodLocations << new Location(spot: spot as int[], naturalResources: resources, score: score)
                 }
-            }
 
-            if (goodLocations) {
                 me.metaObjects[NomadShamanRule.toString()] = goodLocations.max { it.score }
+                return BAD
             }
-
-            return BAD
         }
     }
 
